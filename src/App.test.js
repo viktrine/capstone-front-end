758ea8e60648c0416fdb108ee8ecf316dac456aa
Kipng'etich Victor Lang'at <viclan29@gmail.com>
2023-07-25 09:31:37 +0300

    Pass App test

--- src/App.test.js
+++ src/App.test.js
@@ -1,7 +1,7 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('Home', () => {
+test('C', () => {
   render(<App />);
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
